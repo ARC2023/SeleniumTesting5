e35fed5276121361881cac6c50ec1a42824b11df
ARC2023 <arcautomation2023@gmail.com>
2023-11-25 09:23:37 +0530

    JavaScript Executor

--- src/test/java/day5_HandlingAlert/Alert.java
+++ src/test/java/day5_HandlingAlert/Alert.java
@@ -22,13 +22,13 @@ public class Alert {
 		org.openqa.selenium.Alert alert =   driver.switchTo().alert();
 		
 		// 1. Accept  & // 3. Get Text
-		/*if(alert.getText().equals("Do you really want to delete this Customer?")) {
+		if(alert.getText().equals("Do you really want to delete this Customer?")) {
 			alert.accept();			
 		}
 		Thread.sleep(3000);
 		if(alert.getText().equals("Customer Successfully Delete!")) {
 			alert.accept();			
-		}	*/
+		}	
 		
 		// 2. Dismiss
 		
@@ -43,13 +43,20 @@ public class Alert {
 		alert.sendKeys("Kajal");
 		
 		// 5. Upload Files
+		driver.get("https://html.com/input-type-file/");
 		
+		WebElement upload = driver.findElement(By.xpath("//input[@id='fileupload']"));
 		
+		upload.sendKeys("C:\\Users\\glend\\Downloads\\Manual Interview Questions-Set 1 - Full (1).pdf");
 		
 		// 6. Handling basic Authentication
 
+		driver.get("https://the-internet.herokuapp.com/basic_auth");
+				
+		Thread.sleep(2000);
 		
-		
+		driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+
 	}
 
 }
