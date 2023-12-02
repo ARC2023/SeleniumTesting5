c5d295c7b7cf027df9438bb046ea42948b051c15
ARC2023 <arcautomation2023@gmail.com>
2023-12-02 08:33:01 +0530

    Waits - Completed

--- src/test/java/day11_Waits/AllWaitsInSelenium.java
+++ src/test/java/day11_Waits/AllWaitsInSelenium.java
@@ -25,12 +25,12 @@ public class AllWaitsInSelenium {
 		
 		
 		//1. Visibility		
-		
+		/*
 			WebElement login = wait.until(ExpectedConditions.visibilityOf
 					(driver.findElement(By.id("login_Layer"))));								
 			List<WebElement> dropDownList = wait.until(ExpectedConditions.visibilityOfAllElements
 					(driver.findElements(By.id("login_Layer"))));
-			/*WebElement login1 =	wait.until(ExpectedConditions.visibilityOfElementLocated
+			WebElement login1 =	wait.until(ExpectedConditions.visibilityOfElementLocated
 					(By.xpath("")));
 			List<WebElement> dropDownList1=wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy
 					(By.xpath("")));
@@ -41,7 +41,7 @@ public class AllWaitsInSelenium {
 			visibilityOfNestedElementsLocatedBy(By.id("livePreTable"),By.xpath("[text()='"+companyName+"']")));
 			wait.until(ExpectedConditions.
 			visibilityOfNestedElementsLocatedBy(login1, By.xpath("")));
-				*/
+				
 			
 			// 2. Invisibility
 			
@@ -56,10 +56,52 @@ public class AllWaitsInSelenium {
 			alert.accept();
 			alert.dismiss();
 			alert.getText();
-			
+			*/
 			// 4. Element selection state
 			
+			//driver.get("https://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
 			
+			//WebElement ele= driver.findElement(By.xpath("//input[@value='option-3']"));
+			/*
+			System.out.println(wait.until(ExpectedConditions.elementSelectionStateToBe
+					(By.xpath("//input[@value='option-3']"), true)));
+			System.out.println(wait.until(ExpectedConditions.elementSelectionStateToBe
+					(ele, false)));
+			
+			
+			// 5. Element Clickable State
+			wait.until(ExpectedConditions.elementToBeClickable(ele)).click();
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='cabbage']"))).click();
+			
+			// 6. Element Selected
+			
+			System.out.println(wait.until(ExpectedConditions.elementToBeSelected(ele)));
+			System.out.println(wait.until(ExpectedConditions.elementToBeSelected(By.xpath("//input[@value='option-2']"))));
+			
+			
+			// 7. Frames
+			
+			driver.get("https://demo.guru99.com/test/guru99home/");
+			driver.switchTo().frame("a077aa5e");
+			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("a077aa5e"));
+			driver.findElement(By.xpath("/html/body/a")).click();
+			
+			// 8. Presence of Element
+			
+			wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("")));
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("")));
+			wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(By.xpath(""),By.id("")));
+			wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(ele,By.xpath("")));
+			
+			*/
+			// 9. Text to be present
+			WebElement login= driver.findElement(By.id("login_Layer"));
+			login.click();
+			//System.out.println(wait.until(ExpectedConditions.textToBePresentInElement(login, "Login")));
+			//System.out.println(wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("register_Layer"), "Reegister")));
+			WebElement un=driver.findElement(By.xpath("//*[@id='root']/div[4]/div[2]/div/div/div[2]/div/form/div[2]/input"));
+			un.sendKeys("arc@gmail.com");
+			System.out.println(wait.until(ExpectedConditions.textToBePresentInElementValue(un, "aarc@gmail.com")));
 			
 	}
 
